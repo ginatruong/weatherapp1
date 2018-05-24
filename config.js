da28144713a6fc2eed60fad4c672cb958e8a940c
Gina Truong <gina.y.truong@gmail.com>
2018-05-24 15:58:46 -0500

    made changes to js files

---
+++ config.js
@@ -0,0 +1,7 @@
+//Todo: change the url to be an absolute path, queryString parameter must be passed in as an object
+//queryString paramater, must be passed in as an object to the service
+export const WEATHER_API_LOCATION = 'api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml';
+console.log('Environment', process.env.NODE_ENV);
+export default {
+	name: 'Vince'
+};
--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-import $ from "jquery";
-
-const myArray = [];
-
+import $ from 'jquery';
+import { WEATHER_API_LOCATION } from './config';
 //Retrieving Data and how/where to render
-const WEATHER_APP_API_LOCATION =
-  "api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml";
 
 //Render Results
 function renderResults(list) {
-  return `
+	return `
   <h2>${list.main.temp}</h2>
   <h3>${list.main.humidity}</h3>
   <ul>
@@ -19,7 +15,7 @@ function renderResults(list) {
 }
 
 function renderResults(list) {
-  return `
+	return `
   <h2>${list.main.temp}</h2>
   <h3>${list.main.humidity}</h3>
   <ul>
@@ -31,8 +27,8 @@ function renderResults(list) {
 
 //Display Results
 function displaySearchData(data) {
-  const results = data.list.map((item, index) => renderResults(item));
-  $(".forecast").html(results);
+	const results = data.list.map((item, index) => renderResults(item));
+	$('.forecast').html(results);
 }
 
 //Render Results
