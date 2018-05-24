d7933a4a0e6b129d1afed2e503d287f309f4382d
Gina Truong <gina.y.truong@gmail.com>
2018-05-24 13:08:42 -0500

    added files

---
+++ main.js
@@ -0,0 +1,23 @@
+//Users search for valid city and state
+function searchCity() {
+
+}
+
+//Retrieving Data and how/where to render
+function searchData() {
+
+}
+
+//Render Results
+function renderResults() {
+
+}
+
+//Callback function
+function watchSubmit() {
+searchData(data, renderResults);
+}
+
+//$(watchSubmit);
+
+
