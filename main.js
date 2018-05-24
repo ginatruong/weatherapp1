e1a202a9427db6981a8b3e502952b03697d95d74
Gina Truong <gina.y.truong@gmail.com>
2018-05-24 13:42:04 -0500

    js file functions

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 //Users search for valid city and state
-function searchCity() {
-
-}
+function searchCity() {}
 
 //Retrieving Data and how/where to render
-function searchData() {
+let weatherAppSearch = 'api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml';
 
+function searchData(searchTerm, callback) {
+	const query = {
+		url: weatherAppSearch,
+		data: {
+			q: `${searchTerm} in: main, weather, clouds`
+		},
+		dataType: 'json',
+		type: 'GET',
+		success: callback
+	};
+	$.ajax(query);
 }
 
 //Render Results
-function renderResults() {
-
-}
+function renderResults() {}
 
 //Callback function
 function watchSubmit() {
-searchData(data, renderResults);
+	searchData(data, renderResults);
 }
 
 //$(watchSubmit);
-
-
