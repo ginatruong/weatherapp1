56f656c89a19fb9e7e6bed092f25c2f2e0729802
Vincent St. Louis <vincentst.louis@gmail.com>
2018-05-24 17:31:02 -0400

    refactor

--- config.js
+++ config.js
@@ -1,7 +1,25 @@
 //Todo: change the url to be an absolute path, queryString parameter must be passed in as an object
 //queryString paramater, must be passed in as an object to the service
-export const WEATHER_API_LOCATION = 'api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml';
-console.log('Environment', process.env.NODE_ENV);
+export const WEATHER_API_LOCATION =
+  "api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml";
+
+const myRandomConfig = isDev() ? { place: "dev" } : { place: "prod" };
+
+const myOtherRandomConfig = isProd()
+  ? { place: "isProd('GINA!!!!!')" }
+  : { place: "vince is a noob" };
+
+const CURRENT_ENV = process.env.NODE_ENV;
+
+function isDev() {
+  return CURRENT_ENV === "development";
+}
+
+function isProd() {
+  return CURRENT_ENV === "production";
+}
+
 export default {
-	name: 'Vince'
+  isDev,
+  isProd
 };
