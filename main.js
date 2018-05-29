24c974a8b1c0a0bdab44a4734305a9087a8a1f6b
Gina Truong <gina.y.truong@gmail.com>
2018-05-28 22:05:38 -0500

    made changes for layout

--- main.js
+++ main.js
@@ -1,48 +1,49 @@
-import $ from "jquery";
-import { WEATHER_API_LOCATION, WEATHER_API_KEYS } from "./config"; /// look for const and function
+import $ from 'jquery';
+import { WEATHER_API_LOCATION, WEATHER_API_KEYS } from './config'; /// look for const and function
 
-console.clear();
-console.log("--------- Application Rebundled ---------");
 //Get data from API
 function getDataFromApi(searchTerm, callback) {
-  $.ajax({
-    url: WEATHER_API_LOCATION,
-    data: {
-      //...WEATHER_API_KEYS
-      id: WEATHER_API_KEYS.id,
-      APPID: WEATHER_API_KEYS.APPID,
-      q: `${searchTerm} in:city`
-    },
-    dataType: "json",
-    type: "GET",
-    success: callback
-  });
+	$.ajax({
+		url: WEATHER_API_LOCATION,
+		data: {
+			//...WEATHER_API_KEYS
+			id: WEATHER_API_KEYS.id,
+			APPID: WEATHER_API_KEYS.APPID,
+			q: `${searchTerm} in:city`
+		},
+		dataType: 'json',
+		type: 'GET',
+		success: callback
+	});
 }
 
 // console.log(getTemperatures());
 //Render Results
 function renderResults(list) {
-  return `
-  <h2>${list.main.temp}</h2>
-  <h3>${list.main.humidity}</h3>
-  <ul>
-  <li>${list.weather.main}</li>
-  <li>${list.weather.description}</li>
-  <li>${list.wind.speed}</li>
-  <li>${list.temp_min}</li>
-  <li>${list.temp_max}</li>
-  </ul>`;
+	return `
+	<br>
+  <div class="img-section">	
+	<img src="#" alt="">
+  </div>
+  <div class="main-weather-section">
+  <p class="main-temp" id="outputTemperature">${list.main.temp}</p>
+  <h3 class="humidity">Humidty: ${list.main.humidity}%</h3>
+  <ul class="weather-description">
+  <li>${list.weather[0].description}</li>
+  <li>${list.main.temp_min} | ${list.main.temp_max}</li>
+  </ul>
+  </div>`;
 }
 
 //Display Results
 function displaySearchData(data) {
-  const results = data.list.map((item, index) => renderResults(item));
-  $(".forecast").html(results);
+	const [ first, second, third ] = data.list.map((item, index) => renderResults(item));
+	$('.forecast').html([ first, second, third ]);
 }
 
-$("#searchForm").submit(event => {
-  event.preventDefault();
-  const input = $("#cityInput");
-  getDataFromApi(input.val(), displaySearchData);
-  input.val("");
+$('#searchForm').submit((event) => {
+	event.preventDefault();
+	const input = $('#cityInput');
+	getDataFromApi(input.val(), displaySearchData);
+	input.val('');
 });
