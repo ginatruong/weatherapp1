f283d6daf1e773e7ebca78cbb4287a968ab4851d
Gina Truong <gina.y.truong@gmail.com>
2018-05-30 18:18:32 -0500

    unitofMeasure function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import { WEATHER_API_LOCATION, WEATHER_API_KEYS } from './config'; /// look for const and function
-
+let unitOfMeasure = 'F';
 //Get data from API
 function getDataFromApi(searchTerm, callback) {
 	$.ajax({
@@ -25,7 +25,7 @@ function renderResults(list) {
 	<img src="#" alt="">
   </div>
   <div class="main-weather-section">
-  <p class="main-temp" id="outputTemperature">${list.main.temp}</p>
+  <p class="convert-to-next" data="kelvin" id="outputTemperature">${list.main.temp}</p>
   <h3 class="humidity">Humidty: ${list.main.humidity}%</h3>
   <ul class="weather-description">
   <li>${list.weather[0].description}</li>
@@ -34,27 +34,33 @@ function renderResults(list) {
   </div>`;
 }
 
-let degreeC = '$#8451';
-let degreeF = '$#8457';
+// //converting Temps K to F (original)
+// function convertingTempsF(valNumF) {
+// 	$('#outputTemperature').html((parseFloat(valNumF) - 273.15) * 1.8) + 32;
+// }
 
-//converting Temps K to F (original)
-function convertingTempsF(valNumF) {
-	$('#outputTemperature').html((parseFloat(valNumF) - 273.15) * 1.8) + 32;
-}
+// //converting Temps from F to C
+// function convertingTempsC(valNumC) {
+// 	$('.toggleBtnF').on('click', function(event) {
+// 		valNumC = parseFloat(valNumC);
+// 		$('#outputTemperature').html(valNumC - 32) / 1.8;
+// 	});
+// }
 
-//converting Temps from F to C
-function convertingTempsC(valNumC) {
-	$('.toggleBtnF').on('click', function(event) {
-		valNumC = parseFloat(valNumC);
-		$('#outputTemperature').html(valNumC - 32) / 1.8;
-	});
-}
+// //converting C to F
+// function convertingTempsbacktoF(tempF) {
+// 	$('.toggleBtnC');
+// 	tempF = parseFloat(tempF);
+// 	$('#outputTemperature').html(tempF * 1.8) + 32;
+// }
 
-//converting C to F
-function convertingTempsbacktoF(tempF) {
-	$('.toggleBtnC');
-	tempF = parseFloat(tempF);
-	$('#outputTemperature').html(tempF * 1.8) + 32;
+$('#unitOfMeasure').on('change', function(event) {
+	unitOfMeasure = $(this).val();
+	console.log(unitOfMeasure);
+});
+
+function  () {
+
 }
 
 //Display Results
@@ -62,8 +68,8 @@ function displaySearchData(data) {
 	const [ first, second, third ] = data.list.map((item, index) => renderResults(item));
 	$('.forecast').html([ first, second, third ]);
 }
-
-$('#searchForm').submit((event) => {
+// $('#searchForm').submit -shorthand for below
+$('#searchForm').on('submit', (event) => {
 	event.preventDefault();
 	const input = $('#cityInput');
 	getDataFromApi(input.val(), displaySearchData);
